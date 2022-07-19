8607b14352cbb2398f44d446367b91b89e68f03f
Power Mukisa <power.mukisa@altruist.com>
2022-07-18 18:02:36 -0700

    Initial commit

---
+++ src/main/java/com/fanyacode/fanyacode/FanyacodeApplication.java
@@ -0,0 +1,13 @@
+package com.fanyacode.fanyacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanyacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanyacodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanyacode/fanyacode/FanyacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanyacode.fanyacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanyacodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
